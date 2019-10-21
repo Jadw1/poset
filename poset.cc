391ffa9f818f10b549e5bf34cd9108f67b1658c3
hw394858 <hw394858@mimuw.edu.pl>
2019-10-21 23:06:00 +0100

    struktury

--- poset.cc
+++ poset.cc
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+#include <set>
+#include <map>
+
+using namespace std;
+// id posetu, element, nasz identyfikator
+map < pair< int, string>, int > M;
+
+//id posetu, id, id      //to jest nasz graf. Trzymamy id posetu i zbiór krawędzi.
+map < int, set< pair< int,int > > > G;
+
+//id posetu, zbiór wierzcholków naszych id,
+map < int, set< int > > V;
+
+
+
+
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
