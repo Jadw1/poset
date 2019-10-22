5c4455f2597ef7c77480b023dc1bb8ab913bdaa4
Jadw1 <jadw1_1@o2.pl>
2019-10-22 13:41:19 +0200

    basic structures and helper functions

--- poset.cc
+++ poset.cc
@@ -1,25 +1,45 @@
 #include <iostream>
 #include <string>
-#include <set>
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
 
-using namespace std;
-// id posetu, element, nasz identyfikator
-map < pair< int, string>, int > M;
+namespace {
+    using NodeIndex = unsigned long;
+    // mapa przechowujaca indexy wierzchołków
+    using IndexMap = std::unordered_map<std::string, NodeIndex>;
+    // set indexów do ktorych wskazuje relacja z danego wierzchołka
+    using Edges = std::unordered_set<NodeIndex>;
+    // mapa przechowująca sety wierzchołków do których prowadzi relacja z NodeIndex
+    using PosetGraph = std::unordered_map<NodeIndex, Edges>;
+    // <nodesIndexer, mapa indexów, graf>
+    using Poset = std::variant<NodeIndex, IndexMap, PosetGraph>;
 
-//id posetu, id, id      //to jest nasz graf. Trzymamy id posetu i zbiór krawędzi.
-map < int, set< pair< int,int > > > G;
+    std::unordered_map<unsigned long, Poset> PosetsMap;
+    unsigned long PosetsIndexer = 0;
 
-//id posetu, zbiór wierzcholków naszych id,
-map < int, set< int > > V;
+    std::optional<Poset> getPoset(unsigned long id) {
+        auto el = PosetsMap.find(id);
+        if(el == PosetsMap.end()) {
+            return std::optional(std::nullopt);
+        }
 
+        return std::optional(el->second);
+    }
 
+    IndexMap& getIndexMap(Poset& poset) {
+        return std::get<IndexMap>(poset);
+    }
+
+    PosetGraph& getPosetGraph(Poset& poset) {
+        return std::get<PosetGraph>(poset);
+    }
+}
 
 
 
 
 
 int main() {
-    cout << "Hello, World!" << endl;
     return 0;
 }
