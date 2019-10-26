c0b3bce0709b20bc9b469cfc194129d87267e484
Jadw1 <jadw1_1@o2.pl>
2019-10-26 15:55:57 +0200

    poset_add & bug fixes

--- poset.cc
+++ poset.cc
@@ -129,6 +129,39 @@ namespace {
 
         std::get<NodeIndex>(poset) = 0;
     }
+
+    void addSingleRelation(PosetGraph& graph, NodeIndex index1, NodeIndex index2) {
+        auto& set1 = getRelations(getPosetNode(graph, index1));
+        auto& set2 = getReverseRelations(getPosetNode(graph, index2));
+
+        set1.insert(index2);
+        set2.insert(index1);
+    }
+
+    bool addRelation(Poset& poset, char const* value1, char const* value2) {
+        auto index1 = getNodeIndex(poset, value1);
+        auto index2 = getNodeIndex(poset, value2);
+
+        if(!index1.has_value() || !index2.has_value()) {
+            return false;
+        }
+
+        auto& graph = getPosetGraph(poset);
+
+        auto& extendForwardSet = getRelations(getPosetNode(graph, index2.value()));
+        auto& extendBackwardSet = getReverseRelations(getPosetNode(graph, index1.value()));
+
+        for(auto index : extendForwardSet) {
+            addSingleRelation(graph, index1.value(), index);
+        }
+        for(auto index : extendBackwardSet) {
+            addSingleRelation(graph, index, index2.value());
+        }
+
+        addSingleRelation(graph, index1.value(), index2.value());
+
+        return true;
+    }
 }
 
 unsigned long poset_new() {
@@ -164,8 +197,17 @@ bool poset_insert(unsigned long id, char const *value){
 
     if(poset != nullptr && !isInPoset(*poset, value)) {
         IndexMap& idxMap = getIndexMap(*poset);
-        idxMap.insert({value, id});
+        PosetGraph& graph = getPosetGraph(*poset);
 
+        auto& index = std::get<NodeIndex>(*poset);
+        Edges rel;
+        Edges revRel;
+        Node node(rel, revRel);
+
+        idxMap.insert({value, index});
+        graph.insert({index, node});
+
+        index++;
         return true;
     }
     else {
@@ -181,6 +223,20 @@ bool poset_remove(unsigned long id, char const* value) {
     }
 
     return removeNode(*poset, value);
+}
+
+bool poset_add(unsigned long id, char const* value1, char const* value2) {
+    Poset* poset = getPoset(id);
+
+    if(poset == nullptr) {
+        return false;
+    }
+
+    if(checkRelation(*poset, value1, value2) || checkRelation(*poset, value2, value1)) {
+        return false;
+    }
+
+    return addRelation(*poset, value1, value2);
 }
 
 bool poset_test(unsigned long id, char const* value1, char const* value2) {
@@ -206,10 +262,17 @@ void poset_clear(unsigned long id) {
 
 int main() {
     auto id = poset_new();
-    bool a = poset_insert(id, "qqq");
-    bool b = poset_insert(id, "wrw");
-    bool c = poset_insert(id, "sdf");
-    poset_insert(id, "sdf");
+    bool a = poset_insert(id, "a");
+    bool b = poset_insert(id, "b");
+    bool c = poset_insert(id, "c");
+    poset_insert(id, "d");
+    poset_insert(id, "e");
+
+    poset_add(id, "a", "b");
+    poset_add(id, "b", "c");
+
+    bool res = poset_test(id, "a", "c");
+
     std::cout << poset_size(id);
 
     return 0;
